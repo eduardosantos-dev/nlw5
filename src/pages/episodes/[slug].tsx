59e3cdaa5fb81bcf8c41a43da7bde2431199adcf
Eduardo Santos <eduardosantos.ist@gmail.com>
2021-04-23 22:00:46 -0300

    Aumentando quantidade de páginas carregadas em build

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -67,7 +67,7 @@ export default function Episode({ episode }: EpisodeProps) {
 export const getStaticPaths: GetStaticPaths = async () => {
   const { data } = await api.get("episodes", {
     params: {
-      _limit: 2,
+      _limit: 12,
       _sort: "published_at",
       _order: "desc",
     },
