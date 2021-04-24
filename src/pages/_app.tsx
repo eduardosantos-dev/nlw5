0ce760dda09b30d085bec2a485a948d3877db0f7
Eduardo Santos <eduardosantos.ist@gmail.com>
2021-04-23 21:56:48 -0300

    Aula 5 - Finalizando o app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,39 +1,12 @@
 import "../styles/global.scss";
 import { Header } from "../components/Header";
 import { Player } from "../components/Player";
+import { PlayerContextProvider } from "../contexts/PlayerContext";
 import styles from "../styles/app.module.scss";
-import { PlayerContext } from "../contexts/PlayerContext";
-import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  const [episodeList, setEpisodeList] = useState([]);
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  function play(episode) {
-    setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0);
-    setIsPlaying(true);
-  }
-
-  function togglePlay() {
-    setIsPlaying(!isPlaying);
-  }
-
-  function setPlayingState(state) {
-    setIsPlaying(state);
-  }
-
   return (
-    <PlayerContext.Provider
-      value={{
-        episodeList,
-        currentEpisodeIndex,
-        isPlaying,
-        play,
-        togglePlay,
-        setPlayingState,
-      }}>
+    <PlayerContextProvider>
       <div className={styles.wrapper}>
         <main>
           <Header />
@@ -41,7 +14,7 @@ function MyApp({ Component, pageProps }) {
         </main>
         <Player />
       </div>
-    </PlayerContext.Provider>
+    </PlayerContextProvider>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,10 @@ import { ptBR } from "date-fns/locale";
 import { api } from "../services/api";
 import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
 import Link from "next/link";
+import Head from "next/head";
 
 import styles from "./home.module.scss";
-import { useContext } from "react";
-import { PlayerContext } from "../contexts/PlayerContext";
+import { usePlayer } from "../contexts/PlayerContext";
 
 type Episode = {
   id: string;
@@ -27,14 +27,19 @@ type HomeProps = {
 };
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
-  const { play } = useContext(PlayerContext);
+  const { playList, isPlaying, currentEpisodeIndex } = usePlayer();
+
+  const episodeList = [...latestEpisodes, ...allEpisodes];
 
   return (
     <div className={styles.homepage}>
+      <Head>
+        <title>Home | Podcastr</title>
+      </Head>
       <section className={styles.latestEpisodes}>
         <h2>Últimos lançamentos</h2>
         <ul>
-          {latestEpisodes.map((episode) => {
+          {latestEpisodes.map((episode, index) => {
             return (
               <li key={episode.id}>
                 <Image
@@ -52,7 +57,9 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button" onClick={() => play(episode)}>
+                <button
+                  type="button"
+                  onClick={() => playList(episodeList, index)}>
                   <img src="/play-green.svg" alt="Tocar episódio" />
                 </button>
               </li>
@@ -74,7 +81,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
             </tr>
           </thead>
           <tbody>
-            {allEpisodes.map((episode) => {
+            {allEpisodes.map((episode, index) => {
               return (
                 <tr key={episode.id}>
                   <td style={{ width: 72 }}>
@@ -95,7 +102,11 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <td style={{ width: 100 }}>{episode.publishedAt}</td>
                   <td>{episode.durationAsString}</td>
                   <td>
-                    <button type="button">
+                    <button
+                      type="button"
+                      onClick={() =>
+                        playList(episodeList, index + latestEpisodes.length)
+                      }>
                       <img src="/play-green.svg" alt="Tocar episódio" />
                     </button>
                   </td>
